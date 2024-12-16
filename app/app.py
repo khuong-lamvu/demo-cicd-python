f6bbfd5b81c0ba8f7dc9db886481d88261747c1a
Khuong Lam <lvkhuongnetwork@gmail.com>
2024-12-17 00:42:38 +0700

    Update app.py with return Version 02

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Jenkins CI/CD with Docker Version 02!'
+    return 'Hello, Jenkins CI/CD with Docker Test for Version 02!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)  # Chạy trên cổng 5050
